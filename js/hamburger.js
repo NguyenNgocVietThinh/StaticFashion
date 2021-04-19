295a4d09c4bc4fa59e951f7a0cb93ec850a58d9c
kaizzz2001 <nguyenngocvietthinh@gmail.com>
2021-04-19 09:18:05 +0700

    first commit

---
+++ js/hamburger.js
@@ -0,0 +1,9 @@
+openNav = () => {
+    let nav = document.querySelector('.nav-overlay');
+
+    nav.classList.toggle('active')
+
+    let hamb = document.querySelector('.hamburger')
+
+    hamb.classList.toggle('active')
+}
